6dd9314eb586b02c314e23d44500104217a66717
Skiiicodle <121371082+Skiiidoodle@users.noreply.github.com>
2023-12-23 12:09:30 +0700

    Update spot.py

--- spot.py
+++ spot.py
@@ -11,20 +11,21 @@ SPOTIPY_CLIENT_SECRET= #"YOUR CLIENT SECRET"
 SPOTIPY_REDIRECT_URL="http://127.0.0.1:9090"
 SCOPE="user-top-read"
 
+#Authentication
 sp=spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                              client_secret=SPOTIPY_CLIENT_SECRET,
                                              redirect_uri=SPOTIPY_REDIRECT_URL,
                                              scope=SCOPE))
 
 
-
+#Get user's top track
 def get_tracks_id(time_frame):
     track_ids=[]
     for song in time_frame['items']:
         track_ids.append(song['id'])
     return track_ids
 
-
+#Get track's feature such as artist's name, albumn, track's image and URL
 def get_track_features(id):
     meta=sp.track(id)
     name=meta['name']
@@ -36,7 +37,7 @@ def get_track_features(id):
 
     return track_info
 
-
+#Insert all the track's data into a google sheet
 def insert_to_gsheet(track_ids):
  
     tracks = []
@@ -53,7 +54,7 @@ def insert_to_gsheet(track_ids):
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
     print('Done')
 
-
+#Get 20 tracks from 3 time period ranging from recent most listened track to all time most listened tracks
 time_ranges = ['short_term', 'medium_term', 'long_term']
 for time_period in time_ranges:
     top_tracks = sp.current_user_top_tracks(limit=20, offset=0, time_range=time_period)
