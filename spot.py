6ac1a02661927726f9158602f7aef03998b64c4b
Skiiicodle <121371082+Skiiidoodle@users.noreply.github.com>
2023-12-23 12:05:31 +0700

    Update spot.py

--- spot.py
+++ spot.py
@@ -6,8 +6,8 @@ import time
 import datetime
 import gspread
 
-SPOTIPY_CLIENT_ID ="6780f6cec7ef430494b8782c7407195e"
-SPOTIPY_CLIENT_SECRET="31b6031e44f246d0b85cabf2c1eeec5e"
+SPOTIPY_CLIENT_ID = #"YOUR CLIENT ID"
+SPOTIPY_CLIENT_SECRET= #"YOUR CLIENT SECRET"
 SPOTIPY_REDIRECT_URL="http://127.0.0.1:9090"
 SCOPE="user-top-read"
 
@@ -15,8 +15,6 @@ sp=spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                              client_secret=SPOTIPY_CLIENT_SECRET,
                                              redirect_uri=SPOTIPY_REDIRECT_URL,
                                              scope=SCOPE))
-
-#top_tracks_short=sp.current_user_top_tracks(limit=10, offset=0, time_range="short_term")
 
 
 
@@ -26,10 +24,6 @@ def get_tracks_id(time_frame):
         track_ids.append(song['id'])
     return track_ids
 
-#track_ids= get_tracks_id(top_tracks_short)
-
-
-#track_id='7B3z0ySL9Rr0XvZEAjWZzM'
 
 def get_track_features(id):
     meta=sp.track(id)
@@ -42,26 +36,6 @@ def get_track_features(id):
 
     return track_info
 
-
-
-#tracks=[]
-#for i in range(len(track_ids)):
-    #time.sleep(.5)
-    #track = get_track_features(track_ids[i])
-    #tracks.append(track)
-    #print(f"\n{track}")
-
-#df=pd.DataFrame(tracks, columns=['name', 'album', 'artist', 'spotify_url', 'album_cover'])
-#print(df.head(5))
-
-
-
-#gc=gspread.service_account(filename='spotify-wrapped-408406-9eda9ff82713.json')
-
-#sh= gc.open("Spotify wrapped")
-#worksheet = sh.worksheet("short_term")
-#val = worksheet.acell('B5').value
-#print(val)
 
 def insert_to_gsheet(track_ids):
  
